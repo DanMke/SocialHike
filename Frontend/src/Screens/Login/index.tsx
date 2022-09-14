a4469ee3226117b907ac0b41794b09c47c87a9eb
Daniel Maike <danmke@hotmail.com>
2022-09-14 00:18:55 -0300

    feat: add loading indicator when try to login user

--- Frontend/src/Screens/Login/index.tsx
+++ Frontend/src/Screens/Login/index.tsx
@@ -10,12 +10,18 @@ import {
   Modal,
 } from 'native-base';
 import React from 'react';
-import {View, SafeAreaView, Image, KeyboardAvoidingView} from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  Image,
+  KeyboardAvoidingView,
+  ActivityIndicator,
+} from 'react-native';
 import {connect} from 'react-redux';
 import {updateUser} from '../../Redux/actions';
 
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons';
 
 import SocialHikeIcon from '../../../assets/socialhikeicon.png';
 import auth from '@react-native-firebase/auth';
@@ -30,8 +36,11 @@ interface LoginProps {
   navigation: any;
 }
 
-const Login: React.FC<LoginProps> = ({onUpdateUser, user, navigation}: LoginProps) => {
-
+const Login: React.FC<LoginProps> = ({
+  onUpdateUser,
+  user,
+  navigation,
+}: LoginProps) => {
   const [email, setEmail] = React.useState('');
   const [emailForgotPassword, setEmailForgotPassword] = React.useState('');
   const [password, setPassword] = React.useState('');
@@ -43,29 +52,48 @@ const Login: React.FC<LoginProps> = ({onUpdateUser, user, navigation}: LoginProp
   const [modalErrorMessage, setModalErrorMessage] = React.useState('');
 
   const [showPassword, setShowPassword] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
   const validate = () => {
-    if (email === undefined || email === '' || password === undefined || password === '') {
-      setErrors({...errors, email: 'Email is required', password: 'Password is required'});
+    if (
+      email === undefined ||
+      email === '' ||
+      password === undefined ||
+      password === ''
+    ) {
+      setErrors({
+        ...errors,
+        email: 'Email is required',
+        password: 'Password is required',
+      });
       return false;
-    } 
+    }
 
     return true;
   };
 
   const onSubmit = () => {
     if (validate()) {
+      setLoading(true);
       auth()
         .signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
+        .then(userCredential => {
           console.log('User account signed in!');
           // console.log(userCredential);
-          api.get('/users/' + email).then((response) => {
-            // console.log(response.data);
-            onUpdateUser(response.data);
-            setErrors({});
-            navigation.navigate('TabRoutes',  {screen: 'Home'});
-          });
+          api
+            .get('/users/' + email)
+            .then(response => {
+              // console.log(response.data);
+              onUpdateUser(response.data);
+              setErrors({});
+              console.log('aquiiii');
+              navigation.navigate('TabRoutes', {screen: 'Home'});
+              setLoading(false);
+            })
+            .catch(error => {
+              console.log(error);
+              setLoading(false);
+            });
         })
         .catch(error => {
           const errorCode = error.code;
@@ -83,13 +111,13 @@ const Login: React.FC<LoginProps> = ({onUpdateUser, user, navigation}: LoginProp
             setModalErrorMessage('User not found.');
             setShowModalError(true);
           }
-          
+
+          setLoading(false);
         });
     } else {
       console.log(errors);
       console.log('Validation Failed');
     }
-  
   };
 
   const onForgotPassword = () => {
@@ -124,8 +152,7 @@ const Login: React.FC<LoginProps> = ({onUpdateUser, user, navigation}: LoginProp
         <View style={{width: '100%', height: '100%'}}>
           <View style={styles.containerIcon}>
             <Image source={SocialHikeIcon} style={styles.icon} />
-            <Text fontSize="3xl" 
-            style={styles.appNameText}>
+            <Text fontSize="3xl" style={styles.appNameText}>
               SocialHike
             </Text>
           </View>
@@ -147,13 +174,17 @@ const Login: React.FC<LoginProps> = ({onUpdateUser, user, navigation}: LoginProp
                   onChangeText={value => setEmail(value)}
                 />
                 {'email' in errors && (
-                  <FormControl.ErrorMessage fontSize={'sm'} leftIcon={<WarningOutlineIcon size="sm" />}>
+                  <FormControl.ErrorMessage
+                    fontSize={'sm'}
+                    leftIcon={<WarningOutlineIcon size="sm" />}>
                     {errors.email}
                   </FormControl.ErrorMessage>
                 )}
               </FormControl>
               <FormControl isInvalid={'password' in errors}>
-                <FormControl.Label _text={{fontSize: 'md', color: '#8C8A8C'}} mt="5">
+                <FormControl.Label
+                  _text={{fontSize: 'md', color: '#8C8A8C'}}
+                  mt="5">
                   Password
                 </FormControl.Label>
                 <Input
@@ -165,11 +196,24 @@ const Login: React.FC<LoginProps> = ({onUpdateUser, user, navigation}: LoginProp
                   variant="underlined"
                   borderColor={'#04C37D'}
                   onChangeText={value => setPassword(value)}
-                  type={showPassword ? "text" : "password"} 
-                  InputRightElement={<IconButton icon={<FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} size={20} color="#8C8A8C"/>} onPress={() => setShowPassword(!showPassword)}/>}
+                  type={showPassword ? 'text' : 'password'}
+                  InputRightElement={
+                    <IconButton
+                      icon={
+                        <FontAwesomeIcon
+                          icon={showPassword ? faEye : faEyeSlash}
+                          size={20}
+                          color="#8C8A8C"
+                        />
+                      }
+                      onPress={() => setShowPassword(!showPassword)}
+                    />
+                  }
                 />
                 {'password' in errors && (
-                  <FormControl.ErrorMessage fontSize={'sm'} leftIcon={<WarningOutlineIcon size="sm" />}>
+                  <FormControl.ErrorMessage
+                    fontSize={'sm'}
+                    leftIcon={<WarningOutlineIcon size="sm" />}>
                     {errors.password}
                   </FormControl.ErrorMessage>
                 )}
@@ -181,33 +225,48 @@ const Login: React.FC<LoginProps> = ({onUpdateUser, user, navigation}: LoginProp
                 onPress={() => setShowModal(true)}>
                 Forgot Password?
               </Link>
-              <Button
-                onPress={onSubmit}
-                mt="5"
-                _text={{fontWeight: 'bold', fontSize: 'md', color: '#E9E8E8'}}
-                backgroundColor={'#04AA6C'}
-                paddingTop={4}
-                paddingBottom={4}
-                borderRadius={10}>
-                Log in
-              </Button>
-              <Button
-                onPress={onCreateAnAccount}
-                mt="5"
-                _text={{fontWeight: 'bold', fontSize: 'md', color: '#E9E8E8'}}
-                backgroundColor={'#15573E'}
-                paddingTop={4}
-                paddingBottom={4}
-                borderRadius={10}>
-                Create An Account
-              </Button>
+              {loading ? (
+                <ActivityIndicator size="large" color="#04AA6C" />
+              ) : (
+                <>
+                  <Button
+                    onPress={onSubmit}
+                    mt="5"
+                    _text={{
+                      fontWeight: 'bold',
+                      fontSize: 'md',
+                      color: '#E9E8E8',
+                    }}
+                    backgroundColor={'#04AA6C'}
+                    paddingTop={4}
+                    paddingBottom={4}
+                    borderRadius={10}>
+                    Log in
+                  </Button>
+                  <Button
+                    onPress={onCreateAnAccount}
+                    mt="5"
+                    _text={{
+                      fontWeight: 'bold',
+                      fontSize: 'md',
+                      color: '#E9E8E8',
+                    }}
+                    backgroundColor={'#15573E'}
+                    paddingTop={4}
+                    paddingBottom={4}
+                    borderRadius={10}>
+                    Create An Account
+                  </Button>
+                </>
+              )}
             </VStack>
             <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
               <Modal.Content maxWidth="400px">
                 <Modal.CloseButton />
                 <Modal.Header>Forgot Password?</Modal.Header>
                 <Modal.Body>
-                  Enter email address and we'll send a link to reset your password.
+                  Enter email address and we'll send a link to reset your
+                  password.
                   <FormControl>
                     <FormControl.Label mt={4}>Email</FormControl.Label>
                     <Input
@@ -225,33 +284,39 @@ const Login: React.FC<LoginProps> = ({onUpdateUser, user, navigation}: LoginProp
                 </Modal.Body>
                 <Modal.Footer>
                   <Button.Group space={2}>
-                    <Button backgroundColor={'#15573E'} onPress={() => {
-                    setShowModal(false);
-                    }}>
+                    <Button
+                      backgroundColor={'#15573E'}
+                      onPress={() => {
+                        setShowModal(false);
+                      }}>
                       Cancel
                     </Button>
-                    <Button backgroundColor={'#04AA6C'} onPress={() => {
-                      setShowModal(false);
-                      onForgotPassword();
-                    }}>
+                    <Button
+                      backgroundColor={'#04AA6C'}
+                      onPress={() => {
+                        setShowModal(false);
+                        onForgotPassword();
+                      }}>
                       Send Email
                     </Button>
                   </Button.Group>
                 </Modal.Footer>
               </Modal.Content>
             </Modal>
-            <Modal isOpen={showModalError} onClose={() => setShowModalError(false)}>
+            <Modal
+              isOpen={showModalError}
+              onClose={() => setShowModalError(false)}>
               <Modal.Content maxWidth="400px">
                 <Modal.CloseButton />
                 <Modal.Header>Invalid Data</Modal.Header>
-                <Modal.Body>
-                  {modalErrorMessage}
-                </Modal.Body>
+                <Modal.Body>{modalErrorMessage}</Modal.Body>
                 <Modal.Footer>
                   <Button.Group space={2}>
-                    <Button backgroundColor={'#15573E'} onPress={() => {
-                    setShowModalError(false);
-                    }}>
+                    <Button
+                      backgroundColor={'#15573E'}
+                      onPress={() => {
+                        setShowModalError(false);
+                      }}>
                       Okay
                     </Button>
                   </Button.Group>
