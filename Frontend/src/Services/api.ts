6e037038a342e19360147e72928dad349beb7287
Daniel Maike <danmke@hotmail.com>
2022-09-17 00:19:17 -0300

    Added api ip aws
    Signed-off-by: Daniel Maike <danmke@hotmail.com>

--- Frontend/src/Services/api.ts
+++ Frontend/src/Services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import auth from '@react-native-firebase/auth';
 
 const api = axios.create({
-    baseURL: 'http://192.168.68.100:4000',
+    baseURL: 'http://18.210.32.102:4000',
 });
 
 api.interceptors.request.use(async (config) => {
